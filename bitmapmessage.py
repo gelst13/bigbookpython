498efbbb1f8129b04ba82ee779a98d281303f636
Gelst13 <Gelst13@rambler.ru>
2023-06-02 14:13:19 +0300

    bitmapmessage.py - started

---
+++ bitmapmessage.py
@@ -0,0 +1,12 @@
+"""
+This program uses a multiline string as a bitmap, a 2D image"""
+
+
+def main():
+    print('Bitmap Message, by Al Sweigart al@inventwithpython.com')
+    print("Enter the message to display with the bitmap.")
+    message = input()
+    
+
+if __name__ == '__main__':
+    main()
