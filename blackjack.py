1a856322e0f56180b1f4c571fde96080df13570a
Gelst13 <Gelst13@rambler.ru>
2023-06-05 13:39:13 +0300

    blackjack.py - started

---
+++ blackjack.py
@@ -0,0 +1,36 @@
+"""Blackjack, by Al Sweigart al@inventwithpython.com
+ The classic card game also known as 21. (This version doesn't have
+ splitting or insurance.)
+ More info at: https://en.wikipedia.org/wiki/Blackjack
+ This code is available at https://nostarch.com/big-book-small-python-programming
+ Tags: large, game, card game"""
+
+# Set up the constants:
+HEARTS = chr(9829)  # Character 9829 is '♥'.
+DIAMONDS = chr(9830)  # Character 9830 is '♦'.
+SPADES = chr(9824)  # Character 9824 is '♠'.
+CLUBS = chr(9827)  # Character 9827 is '♣'.
+# (A list of chr codes is at https://inventwithpython.com/charactermap)
+BACKSIDE = 'backside'
+
+def print_intro():
+    print('Blackjack, by Al Sweigart al@inventwithpython.com')
+    print('''Rules:
+      Try to get as close to 21 without going over.
+      Kings, Queens, and Jacks are worth 10 points.
+      Aces are worth 1 or 11 points.
+      Cards 2 through 10 are worth their face value.
+      (H)it to take another card.
+      (S)tand to stop taking cards.
+      On your first play, you can (D)ouble down to increase your bet
+      but must hit exactly one more time before standing.
+      In case of a tie, the bet is returned to the player.
+      The dealer stops hitting at 17.''')
+
+
+def main():
+    print_intro()
+
+
+if __name__ == '__main__':
+    main()
